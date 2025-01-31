48d6241e6531ca73ba91e9226830adc6b3c88270
Patrik Turemark <p.turemark@gmail.com>
2025-01-31 14:13:34 +0100

    made UserRepository, UserCotroller and post in contorller

---
+++ src/main/java/com/example/plantSwap/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.plantSwap.controllers;
+
+import com.example.plantSwap.models.User;
+import com.example.plantSwap.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+}
