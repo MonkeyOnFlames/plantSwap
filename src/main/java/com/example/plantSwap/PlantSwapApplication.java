b25e5d6a6f802a71e49bdd09a780fe46479f8a7d
Patrik Turemark <p.turemark@gmail.com>
2025-01-31 13:16:25 +0100

    first commit

---
+++ src/main/java/com/example/plantSwap/PlantSwapApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantSwapApplication.class, args);
+	}
+
+}
